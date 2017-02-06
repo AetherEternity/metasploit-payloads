8b9a440e5065d98a73ce96ca775548685db6d1fd
Alexey Sintsov <alex.sintsov@gmail.com>
2017-02-06 13:16:40 +0100

    Update dns_server.py

--- python/meterpreter/dns_server.py
+++ python/meterpreter/dns_server.py
@@ -30,8 +30,6 @@ class DomainName(str):
     def __getattr__(self, item):
         return DomainName(item + '.' + self)
 
-
-        
 class DNSTunnelResponse():
     @staticmethod
     def inc(pointer):
